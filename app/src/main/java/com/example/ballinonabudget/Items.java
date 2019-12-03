daf09944fa65df60d3d759e114937e6830583c33
agoldyuk <agoldyuk@iastate.edu>
2019-12-03 16:36:12 -0600

    Items are defined and returnable getters and initilization created

--- app/src/main/java/com/example/ballinonabudget/Items.java
+++ app/src/main/java/com/example/ballinonabudget/Items.java
@@ -1,4 +1,40 @@
 package com.example.ballinonabudget;
 
+/*What are items in this app
+They are purchases
+They have the following qualities
+Name
+Price
+Type
+Hyperlink (Optional)
+* */
 public class Items {
+    private String purchaseName;
+    private double purchaseAmount;
+    private String purchaseCategory;
+    private String hyperlink;
+
+    public Items(String name, double amount, String category, String link){
+        purchaseName = name;
+        purchaseAmount = amount;
+        purchaseCategory = category;
+        hyperlink = link;
+    }
+
+    public String getPurchaseTitle(){
+        return purchaseName;
+    }
+
+    public double getPurchaseAmount(){
+        return purchaseAmount;
+    }
+
+    public String getCategory(){
+        return purchaseCategory;
+    }
+
+    public String getHyperlink(){
+        return hyperlink;
+    }
+    
 }
