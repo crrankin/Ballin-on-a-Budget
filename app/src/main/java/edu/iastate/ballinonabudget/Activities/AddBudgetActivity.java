3d5aed938ce2d05908b23115203b07073fe3bf47
crrankin <crrankin@iastate.edu>
2019-12-04 20:55:45 -0600

    Made the activity for adding a new budget

--- app/src/main/java/edu/iastate/ballinonabudget/Activities/AddBudgetActivity.java
+++ app/src/main/java/edu/iastate/ballinonabudget/Activities/AddBudgetActivity.java
@@ -1,5 +1,6 @@
 package edu.iastate.ballinonabudget.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,10 +10,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.iastate.ballinonabudget.DatabaseConfig.AppDatabase;
+import edu.iastate.ballinonabudget.Objects.Budget;
+import edu.iastate.ballinonabudget.Objects.Items;
 import edu.iastate.ballinonabudget.R;
 
 public class AddBudgetActivity extends AppCompatActivity {
+
+    private AppDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +31,41 @@ public class AddBudgetActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        database = AppDatabase.getAppDatabase(this);
+    }
+
+    /**
+     * This method runs when a user decides to stop
+     * creating a new budget. This will take the user back
+     * to the home screen.
+     * @param view
+     */
+    public void cancelBudget(View view){
+        Intent goHome = new Intent(this, MainActivity.class);
+        startActivity(goHome);
+    }
+
+    /**
+     * This method runs when a user creates a new Budget
+     * @param view
+     */
+    public void addBudget(View view){
+        //Collects the data from views
+        EditText nameText = findViewById(R.id.nameInput);
+        EditText amountText = findViewById(R.id.amountInput);
+
+        //create a new Budget object using the variables we just made
+        String name = nameText.getText().toString();
+        double amount = Double.parseDouble(amountText.getText().toString());
+        List<Items> items = new ArrayList<>(); //start with a blank items/purchases list
+        Budget b = new Budget(name, amount, items);
+
+        //insert new budget into our database
+        database.budgetDao().insertBudget(b);
+
+        //now go back to home screen
+        Intent goHome = new Intent(this, MainActivity.class);
+        startActivity(goHome);
     }
 
 }
