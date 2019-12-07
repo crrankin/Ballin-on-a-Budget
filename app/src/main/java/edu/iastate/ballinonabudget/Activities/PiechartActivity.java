65d71ec9e261e18a5d9b1b9b0528e85a5d10d0a4
Ricardo Faure <rjfaure@iastate.edu>
2019-12-07 15:25:30 -0600

    Most base functionalities completed

--- app/src/main/java/edu/iastate/ballinonabudget/Activities/PiechartActivity.java
+++ app/src/main/java/edu/iastate/ballinonabudget/Activities/PiechartActivity.java
@@ -2,15 +2,30 @@ package edu.iastate.ballinonabudget.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import edu.iastate.ballinonabudget.DatabaseConfig.AppDatabase;
+import edu.iastate.ballinonabudget.Objects.Budget;
 import edu.iastate.ballinonabudget.R;
 
 public class PiechartActivity extends AppCompatActivity {
+
+    private AppDatabase database;
+    private Budget selectedBudget;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piechart);
+
+        database = AppDatabase.getAppDatabase(this);
+
+        Intent intent = getIntent();
+        int id = intent.getIntExtra("uid", 0);
+        selectedBudget = database.budgetDao().findByID(id);
+
+        //Found a handy tutorial for making Pie Charts:)
+        //https://www.codingdemos.com/android-pie-chart-tutorial/
     }
 }
