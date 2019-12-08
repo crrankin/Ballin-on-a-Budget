a65fb2ab28a9b705d8eb0520a9911c0ae6948c8a
agoldyuk <agoldyuk@iastate.edu>
2019-12-08 12:15:23 -0600

    Item gets passed date and time with a nifty calendar view ain't it swell~

--- app/src/main/java/edu/iastate/ballinonabudget/Activities/AddItemsActivity.java
+++ app/src/main/java/edu/iastate/ballinonabudget/Activities/AddItemsActivity.java
@@ -10,8 +10,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import edu.iastate.ballinonabudget.DatabaseConfig.AppDatabase;
 import edu.iastate.ballinonabudget.Objects.Budget;
@@ -25,6 +31,7 @@ public class AddItemsActivity extends AppCompatActivity {
     private EditText nameText;
     private EditText amountText;
     private EditText hrefText;
+    private CalendarView calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +41,15 @@ public class AddItemsActivity extends AppCompatActivity {
         nameText = findViewById(R.id.nameInputItems);
         amountText = findViewById(R.id.amountInput);
         hrefText = findViewById(R.id.hyperlinkInput);
+        calendar = findViewById(R.id.calendarView);
 
         database = AppDatabase.getAppDatabase(this);
         Intent intent = getIntent();
         int id = intent.getIntExtra("uid", 0);
         selectedBudget = database.budgetDao().findByID(id);
+
+        calendar.setDate(Calendar.getInstance().getTimeInMillis(),false,true);
+
     }
 
     public void cancelItem(View view) {
@@ -52,8 +63,11 @@ public class AddItemsActivity extends AppCompatActivity {
         double amount = Double.parseDouble(amountText.getText().toString());
         String href = hrefText.getText().toString();
 
+        String monthString  = (String) DateFormat.format("MMM",  calendar.getDate());
+        String year         = (String) DateFormat.format("yyyy", calendar.getDate());
+
         //Needs to be updated to include month, recurring, year and category.
-        Items item = new Items(name, amount, "", href, false, "", "");
+        Items item = new Items(name, amount, "", href, false, monthString, year);
 
         selectedBudget.addItem(item);
 
